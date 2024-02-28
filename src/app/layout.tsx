745996a3b9f2291d37a5862a8cd6a033dde0d284
bwsrcurtis <bwsrcurtis@gmail.com>
2024-02-27 23:15:32 -0500

    Added autoformatting

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,22 @@
-import type { Metadata } from "next";
-import { Inter, Roboto_Mono } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next';
+import { Inter, Roboto_Mono } from 'next/font/google';
+import './globals.css';
 
 const inter = Inter({
   subsets: ['latin'],
   display: 'swap',
   variable: '--font-inter',
-})
- 
+});
+
 const roboto_mono = Roboto_Mono({
   subsets: ['latin'],
   display: 'swap',
   variable: '--font-roboto-mono',
-})
+});
 
 export const metadata: Metadata = {
-  title: "Poetry.AI",
-  description: "Generate Custom Poetry With AI Assistance",
+  title: 'Poetry.AI',
+  description: 'Generate Custom Poetry With AI Assistance',
 };
 
 export default function RootLayout({
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,39 +1,42 @@
-"use client";
-import { useState } from "react";
+'use client';
+import { useState } from 'react';
 import parse from 'html-react-parser';
-import Image from "next/image";
+import Image from 'next/image';
 
 export default function Home() {
-  const [theInput, setTheInput] = useState("");
-  const [typeOfPoem, setTypeOfPoem] = useState("")
-  const [moodOfPoem, setMoodOfPoem] = useState("")
-  const [topicOfPoem, setTopicOfPoem] = useState("")
+  const [theInput, setTheInput] = useState('');
+  const [typeOfPoem, setTypeOfPoem] = useState('');
+  const [moodOfPoem, setMoodOfPoem] = useState('');
+  const [topicOfPoem, setTopicOfPoem] = useState('');
   const [isLoading, setIsLoading] = useState(false);
   const [messages, setMessages] = useState([
     {
-      role: "assistant",
-      content: "Tell me the type of poem you would like me to write, how you would like it to make you feel, and what it should be about.",
+      role: 'assistant',
+      content:
+        'Tell me the type of poem you would like me to write, how you would like it to make you feel, and what it should be about.',
     },
   ]);
-  const [output, setOutput] = useState("")
+  const [output, setOutput] = useState('');
   // below this
 
-  
   const callGetResponse = async () => {
     setIsLoading(true);
     let temp = messages;
-    temp.push({ role: "user", content: `Write me a poem with a title with these guidelines. Output title as html h1 tag and lines as p tags. 
-    Type: ${typeOfPoem} Subject: ${topicOfPoem}  Mood: ${moodOfPoem}` });
+    temp.push({
+      role: 'user',
+      content: `Write me a poem with a title with these guidelines. Output title as html h1 tag and lines as p tags.
+    Type: ${typeOfPoem} Subject: ${topicOfPoem}  Mood: ${moodOfPoem}`,
+    });
     setMessages(temp);
-    setTypeOfPoem("");
-    setMoodOfPoem("");
-    setTopicOfPoem("");
-    console.log("Calling OpenAI...");
+    setTypeOfPoem('');
+    setMoodOfPoem('');
+    setTopicOfPoem('');
+    console.log('Calling OpenAI...');
 
-    const response = await fetch("/api", {
-      method: "POST",
+    const response = await fetch('/api', {
+      method: 'POST',
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
 
       body: JSON.stringify({ messages }),
@@ -41,15 +44,15 @@ export default function Home() {
 
     const data = await response.json();
     const { output } = data;
-    console.log("OpenAI replied...", output.content);
+    console.log('OpenAI replied...', output.content);
 
     // setMessages((prevMessages) => [...prevMessages, output]);
-    setOutput(output.content)
+    setOutput(output.content);
     setIsLoading(false);
   };
 
   const Submit = (event: React.KeyboardEvent<HTMLTextAreaElement>) => {
-    if (event.key === "Enter") {
+    if (event.key === 'Enter') {
       event.preventDefault();
       callGetResponse();
     }
@@ -57,48 +60,48 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center overflow-hidden">
-      <Image src='/poetryAILogo.svg' width={150} height={150} alt="Logo" className='absolute top-0 -z-10'/>
-      <h1 className="text-4xl sm:text-6xl items-center justify-center font-mono text-tertiary text-center tracking-wider pt-9 h-36 bg-background/15 w-full">Poetry.AI</h1>
+      <Image
+        src="/poetryAILogo.svg"
+        width={150}
+        height={150}
+        alt="Logo"
+        className="absolute top-0 -z-10"
+      />
+      <h1 className="text-4xl sm:text-6xl items-center justify-center font-mono text-tertiary text-center tracking-wider pt-9 h-36 bg-background/15 w-full">
+        Poetry.AI
+      </h1>
 
-      <div className="flex py-2  h-[35rem] min-w-[350px] w-[80%] max-w-[1500px] flex-col font-sans items-center bg-tertiary/25 rounded-xl drop-shadow-xl">
+      <div className="flex py-2  h-[35rem] min-w-[350px] w-[90%] sm:w-[80%] max-w-[1500px] flex-col font-sans items-center bg-tertiary/25 rounded-xl drop-shadow-xl">
         <div className=" h-full flex flex-col gap-2 overflow-y-auto py-8 px-3 w-full">
           {messages.map((e) => {
             return (
               <div
                 key={e.content}
                 className={`w-[80%] rounded-md px-4 py-3 h-fit ${
-                  e.role === "assistant"
-                    ? "self-center  bg-primary/75 text-gray-800"
-                    : "hidden"
+                  e.role === 'assistant' ? 'self-center  bg-primary/75 text-gray-800' : 'hidden'
                 } `}
               >
                 {e.content}
               </div>
             );
           })}
-              <div
-                className={`w-[80%] rounded-md px-4 py-3 h-min ${
-                  output != ""
-                  ? "self-center bg-secondary/25 text-black text-center"
-                  : "hidden"
-                } `}
-              >
-                {parse(output)}
-                
-              </div>
-            
-          
-
+          <div
+            className={`w-[80%] rounded-md px-4 py-3 h-min ${
+              output != '' ? 'self-center bg-secondary/25 text-black text-center' : 'hidden'
+            } `}
+          >
+            {parse(output)}
+          </div>
 
           {isLoading ? (
-            <div className="self-center  bg-background text-black w-max max-w-[18rem] rounded-md px-4 py-3 h-min">
+            <div className="self-center bg-secondary text-black w-max max-w-[18rem] rounded-md px-4 py-3 h-min">
               *thinking*
             </div>
           ) : (
-            ""
+            ''
           )}
         </div>
-        <div className="relative bottom-4 flex items-end justify-center sm:gap-1 h-[35rem] min-w-[350px] w-[80%]">
+        <div className="relative bottom-4 flex items-end justify-center px-2 mt-5 gap-1 h-[fit-content] min-w-[350px] w-[90%] sm:w-[80%] max-w-[1500px] ">
           <textarea
             value={typeOfPoem}
             onChange={(event) => setTypeOfPoem(event.target.value)}
@@ -124,12 +127,12 @@ export default function Home() {
             onKeyDown={Submit}
           />
         </div>
-          <button
-            onClick={callGetResponse}
-            className="w-[fit-content] bg-background text-text px-4 py-2 rounded mb-2 hover:bg-tertiary hover:text-background hover:px-6"
-          >
-            Generate
-          </button>
+        <button
+          onClick={callGetResponse}
+          className="w-[fit-content] bg-secondary text-text px-4 py-2 rounded mb-2 hover:bg-tertiary hover:text-background hover:px-6"
+        >
+          Generate
+        </button>
       </div>
 
       <div></div>
